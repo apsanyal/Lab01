0c2f70b7ff37e3a33de1240a4f3fb9bd165a56a1
AP <apratimsanyal@gmail.com>
2023-02-06 23:58:54 -0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/* This is my initial basic JavaScript code for Lab01     */
+
+function positiveAffirmation() {
+    const name = document.querySelector("input.name").value;
+    const message = document.querySelector("textarea").value;
+
+    alert("Hello, " + name + "! " + message);
+}
